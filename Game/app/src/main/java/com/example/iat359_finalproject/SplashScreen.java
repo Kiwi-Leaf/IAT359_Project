cb9bed3a613f372c2c2349b6aba3e08459b18ccf
Kiwi-Leaf <kibiyip@gmail.com>
2022-11-13 18:18:47 -0800

    Replaced splash background

--- Game/app/src/main/java/com/example/iat359_finalproject/SplashScreen.java
+++ Game/app/src/main/java/com/example/iat359_finalproject/SplashScreen.java
@@ -20,6 +20,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //Hide the action bar on top
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
