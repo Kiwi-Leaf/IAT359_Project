88880ff7531e5fa7f5f79fbd288f4396ca0a0142
Kiwi-Leaf <kibiyip@gmail.com>
2022-11-13 16:36:34 -0800

    Added Splash Screen and test
    Tested on how to fill background

---
+++ Game/app/src/main/java/com/example/iat359_finalproject/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.iat359_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+    ImageView backgroundIV;
+    final String DEBUG= "Splash debug";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_splash_screen);
+        backgroundIV=findViewById(R.id.backgroundIV);
+
+    }
+
+    public void startGame(View v){
+        Intent start =new Intent(this, MainActivity.class);
+        startActivity(start);
+    }
+}
