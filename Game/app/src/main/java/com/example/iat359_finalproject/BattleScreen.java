4f405cd1d8326a7e5173a9e60bb13e524eb7f88a
Kiwi-Leaf <kibiyip@gmail.com>
2022-11-17 21:44:16 -0800

    Added background to battle screen

--- Game/app/src/main/java/com/example/iat359_finalproject/BattleScreen.java
+++ Game/app/src/main/java/com/example/iat359_finalproject/BattleScreen.java
@@ -2,7 +2,9 @@ package com.example.iat359_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BattleScreen extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class BattleScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle_screen);
+    }
+
+    public void gotoSetting (View v){
+        Intent setting = new Intent(this, SettingScreen.class);
+        startActivity(setting);
     }
 }
